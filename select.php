bd29cc0fe513ee0c2e3f96f8f1921cdf573e041d
marouaneMOQRANE <127626651+marouaneMOQRANE@users.noreply.github.com>
2024-03-23 22:49:00 +0000

    Update select.php

--- select.php
+++ select.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sélection du nombre de tentatives</title>
+     <title>Jeu de devinette de nombres</title>
     <style>
         body {
             font-family: Arial, sans-serif;
